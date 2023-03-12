db5de8e3df69418b18f584fb51a6c5e2a751b540
charchar111 <114625601+charchar111@users.noreply.github.com>
2023-03-12 10:50:01 +0900

    ver1.1
    bug not fix

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const headerMenuBtn = document.querySelector(
   ".header-nav__li1 .header-nav__item1 i"
 );
-console.dir(headerMenuBtn);
+const body = document.body;
+const apps = document.querySelector(".apps");
 
-function headerMenuBtnOpen() {
-  console.dir("hi");
+const mainMenu = document.querySelector("div.menu");
+const mainMenuXButton = mainMenu.querySelector(".menu__bar-x");
+
+console.dir(mainMenuXButton);
+
+function appsPositionFix() {
+  overflow = body.style.overflow;
+  if (overflow !== "hidden") {
+    body.style.overflow = "hidden";
+    body.style.paddingRight = "16px";
+    apps.classList.toggle("apps--opacity-down");
+  } else {
+    body.style.overflow = "";
+    body.style.paddingRight = "";
+    apps.classList.toggle("apps--opacity-down");
+  }
 }
 
-headerMenuBtn.addEventListener("click", headerMenuBtnOpen);
+function headerMenuBtnhandle() {
+  appsPositionFix();
+  console.dir("start");
+  console.dir(mainMenu.style.left);
+  if (mainMenu.classList.contains("menu-close")) {
+    mainMenu.classList.remove("menu-close");
+  }
+
+  open = mainMenu.classList.toggle("menu-open");
+  console.dir(open);
+  if (open === true) {
+    mainMenu.style.left = "0";
+  } else {
+    mainMenu.style.left = "";
+    mainMenu.classList.toggle("menu-close");
+  }
+  console.log("finish");
+}
+
+headerMenuBtn.addEventListener("click", headerMenuBtnhandle);
+mainMenuXButton.addEventListener("click", headerMenuBtnhandle);
